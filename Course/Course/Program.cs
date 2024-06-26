acaad8167e9d617b4264ca099b43929bef90f6a4
Hector Ortiz <hectorluortiz@gmail.com>
2024-06-25 22:02:30 -0300

    25/06

---
+++ Course/Course/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            sbyte n3 = sbyte.MinValue;
+            decimal n4 = decimal.MaxValue;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+
+
+        }                         
+    }
+}
